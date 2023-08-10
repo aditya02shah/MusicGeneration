070f853b49b6ec25e2967fe5969c1560fc3b282b
Aditya <aditya02shah@gmail.com>
2023-08-10 21:21:49 +0530

    Updating Files

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, request, jsonify
 import tensorflow as tf
-from utils import *
+
 
 
 app = Flask(__name__)
+
+vocab=['\n', ' ', '!', '"', '#', "'", '(', ')', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', '<', '=', '>', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', ']', '^', '_', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '|']
+char_to_index = dict((j,i) for i,j in enumerate(vocab))
+
+index_to_char = dict((i,j) for i,j in enumerate(vocab))
+
 def load_model():
     model=tf.keras.models.load_model('LSTM_model.h5')
     return model
